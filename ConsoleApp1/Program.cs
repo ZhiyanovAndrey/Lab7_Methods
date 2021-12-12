866f7cf5a357b9bf36ff2c7633263e1fec7c3d7f
ZhiyanovAndrey <aazhiyanov@gmail.com>
2021-12-12 18:11:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = -1, y1 = 1;
+            int x2 = 11, y2 = 5;
+            int x3 = 0, y3 = 20;
+            double firstSide = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            double secondSide = Math.Sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+            double thirdSide = Math.Sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
+            double p = (firstSide + secondSide + thirdSide) / 2;
+            double Square = Math.Sqrt(p * (p - firstSide) * (p - secondSide) * (p - thirdSide));
+            double Perimeter = firstSide + secondSide + thirdSide;
+
+
+
+            Console.WriteLine("Периметр прямоугольника равен: " + Perimeter);
+            Console.WriteLine("Плошадь прямоугольника равна: " + Square);
+            Console.WriteLine("Стороны {0}\t{1}\t{2}", +firstSide, +secondSide, +thirdSide);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Lab7_Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Создать метод для вычисления объема и площади поверхности куба по длине его ребра. 
+namespace Lab7_Methods
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число больше нуля ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            float S, V;
+            CountCubeVolume_Square(a, out S, out V);
+            Console.WriteLine(S);
+            Console.WriteLine(V);
+            Console.ReadKey();
+        }
+       
+        static int CountCubeVolume_Square(float a, out float S, out float V)
+        {
+
+            V = a * a * a;
+            S = 6 * a * a;
+            return S,V;
+        }
+    }
+}
